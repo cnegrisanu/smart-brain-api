4b331013ced72b540d909c5afb8899aec26d2e77
Cristian Negrisanu <vollucris@gmail.com>
2018-08-08 17:13:49 +0200

    Add API call to server from frontend.Addition to cnegrisanu/smart-brain#2

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,19 @@
+const Clarifai = require('clarifai');
+
+
+const app = new Clarifai.App({
+    apiKey: '681195e7b95b49638874121ead95f132'
+   });
+
+const handleApiCall = (req,res) => {
+   app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+   .then(data => {
+       res.json(data);
+   })
+   .catch(err => res.status(400).json('Unable to work with API', err));
+}
+
+
 const handleImage = (req, res, db) => {
     const {id} = req.body;
 
@@ -5,9 +21,10 @@ const handleImage = (req, res, db) => {
     .then(response => {
         res.json(response[0]);
     })
-    .catch(err => res.status(400).json('Unable to get entries!'));
+    .catch(err => res.status(400).json('Unable to get entries!', err));
 }
 
 module.exports = {
-    handleImage: handleImage
+    handleImage: handleImage,
+    handleApiCall: handleApiCall
 }
